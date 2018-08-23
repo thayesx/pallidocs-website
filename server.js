db439d1b026166f29fc8b1b8d825f3d5c6093be4
Tyler Hayes <tylerxhayes@gmail.com>
2018-08-23 16:37:48 -0700

    basic content layout with video, revised server

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-const express = require('express');
-const server = express();
+// Initialize requirements
+let express = require("express");
+let sass = require("node-sass-middleware");
 
-// server.get('/', (req, res) => res.send('Hello World!'));
+// Initialize variables
+let path = "site/";
 
-server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// Run Server: runs all server tasks
+let runServer = function() {
+  // Setup server
+  let server = express();
+  server.listen(3000, () => console.log('Pallidocs website running on port 3000', __dirname));
 
-server.listen(3000, () => console.log('Example app listening on port 3000!'));
+  // Compile SASS
+  server.use(
+    sass({
+      src: path,
+      dest: path + "css/",
+      debug: true,
+      outputStyle: "compressed",
+      prefix: "/css/",
+    })
+  );
+
+  // Start Express static
+  server.use(express.static(path));
+
+  // Load base HTML
+  server.get('/', (req, res) => {
+    res.sendFile(path + 'index.html');
+  });
+};
+
+// Run server
+runServer();
