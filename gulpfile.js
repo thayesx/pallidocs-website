524a57c9d8e4c74211e14752aa112c56e050702f
Tyler Hayes <tylerxhayes@gmail.com>
2019-01-25 12:13:17 -0800

    update gulpfile and rubies

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,21 @@
-var gulp = require('gulp');
-var minify = require('gulp-minify');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const minify = require('gulp-minify');
+var concat = require('gulp-concat');
+const filter = require('gulp-filter');
 
-gulp.task('copyLibraries', function () {
-	var sources = [
-		'./bower_components/jquery/dist/jquery.min.js',
-		'./bower_components/jquery-mask-plugin/dist/jquery.mask.min.js'
-	];
-    gulp.src(sources)
-        .pipe(gulp.dest('./js/'));
-});
+const paths = {
+  scripts: {
+    src: 'src/*.js',
+    dest: 'scripts/'
+  }
+};
 
-gulp.task('minifyJS', function () {
-	var sources = ['./js/main.js'];
-	gulp.src(sources)
-		.pipe(minify({
-			ext: {
-				min: '.min.js',
-			},
-			mangle: false,
-		}))
-		.pipe(gulp.dest('./js/'));
-});
+function scripts() {
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(minify({ext: ".min.js"}))
+    .pipe(filter('**/*.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
 
-gulp.task('minifyCSS', function () {
-	var sources = ['./css/fontawesome.css'];
-	gulp.src(sources)
-	    .pipe(cleanCSS())
-		.pipe(rename({
-	    	suffix: '.min'
-	    }))
-	    .pipe(gulp.dest('./css/'));
-});
-
-gulp.task('default', ['copyLibraries', 'minifyJS', 'minifyCSS']);
+gulp.task('default', scripts);
